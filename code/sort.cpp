d0be73db8e05008f3da44ef49ef316183f012c01
Enricky Hipólito <enricky.hipolito@gmail.com>
2022-04-26 21:40:36 -0300

    coding sort.cpp
    an algorithm that get a array and sort it from the smallest to the bigger one

---
+++ code/sort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int sortArray(int array[], int elementsInArray){
+  sort(array, array + elementsInArray);
+  return 0;
+}
+
+int main(){
+  int array[] = {4, 56, 6, 7, -65, 23, 413, -2, 5, 7};
+
+  int elementsInArray = sizeof(array) / sizeof(array[0]);
+  //sizeof(array) get to size of the array in bytes (4 bytes per element)
+  //sizeof(array[0] get the size of each element
+  //elementInArray is the total elements in array.
+
+  cout << "sizeof array: " << sizeof(array) << endl;
+  cout << "sizeof array[0]: " << sizeof(array[0]) << endl;
+  cout << "elementsInArray: " << elementsInArray << endl;
+
+  sortArray(array, elementsInArray);
+ 
+  for (int i = 0; i < elementsInArray; i++){
+    cout << array[i] << " ";
+  }
+
+  return 0;
+}
